da448f7058b7f7ae38899a02b4dfa20c10bedf97
Travis Haas <tghaas@gmail.com>
2014-08-26 09:00:48 -0400

    added dns zone

--- manifests/dns01.pp
+++ manifests/dns01.pp
@@ -1,3 +1,23 @@
 
 include ntp
 include ldap
+include bind
+bind::server::conf { '/etc/named.conf':
+  listen_on_addr    => [ 'any' ],
+  listen_on_v6_addr => [ 'any' ],
+  forwarders        => [ '8.8.8.8', '8.8.4.4' ],
+  allow_query       => [ 'localnets' ],
+  allow_transfer	=> [ 'localhost','10.25.70.16'],
+  zones             => {
+    'topcoder.com' => [
+      'type master',
+      'file "named.topcoder"',
+    ],
+  },
+}
+bind::server::file { 'named.topcoder':
+  source => 'puppet:///modules/config_files/named.topcoder',
+}
+class { 'timezone':
+    timezone => 'America/New_York',
+}
