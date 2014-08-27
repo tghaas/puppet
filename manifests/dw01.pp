815f619c786e1cd5beaee02891506affe3284f3b
Travis Haas <tghaas@gmail.com>
2014-08-27 15:54:44 -0400

    added dw01

---
+++ manifests/dw01.pp
@@ -0,0 +1,10 @@
+class { 'java':
+  java_archive  => 'jdk-8u5-linux-x64.tar.gz',
+    java_home   => '/usr/local/java/jdk1.8.0_05/',
+    java_folder => 'jdk1.8.0_05'
+}
+include ntp
+include ldap
+class { 'timezone':
+    timezone => 'America/New_York',
+}
