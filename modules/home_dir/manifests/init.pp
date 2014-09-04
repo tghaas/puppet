bd63b842d68e1ed4c8585f6b256e869ecec0f8f1
Travis Haas <tghaas@gmail.com>
2014-09-04 09:16:31 -0400

    removed extra files

---
+++ modules/home_dir/manifests/init.pp
@@ -0,0 +1,15 @@
+# Purpose: Extract Home Directory
+#
+# Override defaults in the node manifest:
+#
+#
+#class { 'home_dir':
+#  home_dir_archive => 'tcnode2.20140819.tgz',
+#  home_dir_owner   => 'web',
+#  home_directory   => '/home/web',
+#  home_dir_group   => 'apps'
+#}
+class home_dir
+  {
+  
+}
