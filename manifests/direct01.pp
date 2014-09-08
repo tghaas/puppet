23cb92b89f95166ef148444b64772b888444189c
Travis Haas <tghaas@gmail.com>
2014-09-08 16:09:33 -0400

    new node

---
+++ manifests/direct01.pp
@@ -0,0 +1,27 @@
+include java
+java::jdk { 'jdk6.18':
+  java_archive => 'jdk-6u18-linux-x64.tar.gz',
+  java_home    => '/usr/local/java/jdk1.6.0_18_x64/',
+  java_folder  => 'jdk1.6.0_18_x64';
+}
+include ntp
+include ldap
+include home_dir
+include download
+
+download::file {
+  '/tmp/direct.20140904.tgz':
+    uri     => 'http://10.25.45.226/direct.20140904.tgz',
+    timeout => 900;
+
+}
+home_dir::dir { 'direct01':
+  home_dir_archive => 'direct.20140904.tgz',
+  home_dir_owner   => 'direct',
+  home_directory   => '/home/direct',
+  home_dir_group   => 'apps';
+}
+
+class { 'timezone':
+    timezone => 'America/New_York',
+}
