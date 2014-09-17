22fb4856a5170f3139bb4bf619412285b33e6b3d
Travis Haas <tghaas@gmail.com>
2014-09-17 13:03:17 -0400

    added wget to be sure it was installed

--- modules/download/manifests/file.pp
+++ modules/download/manifests/file.pp
@@ -1,10 +1,12 @@
-  define download::file ($uri, $timeout = 300) {
-
-      exec {
-          "download $uri":
-              command => "/usr/bin/wget -q '$uri' -O $name",
-              creates => $name,
-              timeout => $timeout,
-              require => Package[ "wget" ],
-      }
+define download::file ($uri, $timeout = 300) {
+  package { "wget": 
+    ensure => installed,
   }
+  exec {
+    "download $uri":
+      command => "/usr/bin/wget -q '$uri' -O $name",
+      creates => $name,
+      timeout => $timeout,
+      require => Package["wget"],
+  }
+}
