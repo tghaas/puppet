95e4d4070188e504eae54ce4185acfe4f521b281
Travis Haas <tghaas@gmail.com>
2014-09-17 17:36:36 -0400

    updated config

--- modules/download/manifests/file.pp
+++ modules/download/manifests/file.pp
@@ -1,7 +1,4 @@
 define download::file ($uri, $timeout = 300) {
-  package { "wget": 
-    ensure => installed,
-  }
   exec {
     "download $uri":
       command => "/usr/bin/wget -q '$uri' -O $name",
